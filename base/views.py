9cfe0d9451be79090b2b99a5dc58c49ed70510ca
Andhar7 <boxingprofi@web.de>
2025-07-08 22:43:41 +0200

    Database & Admin Panel,model - Room

---
+++ base/views.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Room
+
+'''
+rooms = [
+    {'id': 1, 'name': 'Peace Run News'},
+    {'id': 2, 'name': '3100 mile Race'},
+    {'id': 3, 'name': 'Looking for a job'},
+]
+'''
+
+def home(request):
+    # return HttpResponse("Welcome to the home page!")
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=int(pk))
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+'''
+def room(request, pk):
+    # return HttpResponse("Welcome to the room page!")
+    room = None
+    rooms = Room.objects.all()
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ commands.md
@@ -0,0 +1,12 @@
+
+
+python3 manage.py startapp base
+
+python3 manage.py migrate 
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate 
+
+python3 manage.py createsuperuser
+
