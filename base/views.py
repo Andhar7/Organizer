30fbd19191b50a98ef3b2c69225d0c32bcbad826
Andhar7 <boxingprofi@web.de>
2025-07-11 00:23:39 +0200

    second step

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Room
+from . forms import RoomForm
 
 '''
 rooms = [
@@ -10,16 +11,25 @@ rooms = [
 ]
 '''
 
+
 def home(request):
     # return HttpResponse("Welcome to the home page!")
     rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
     room = Room.objects.get(id=int(pk))
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def create_room(request):
+    form = RoomForm()
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
 
 '''
 def room(request, pk):
@@ -33,19 +43,3 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 '''
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- commands.md
+++ commands.md
@@ -10,3 +10,17 @@ python3 manage.py migrate
 
 python3 manage.py createsuperuser
 
+python3 manage.py runserver
+
+# After creating of Topic and Message models, run the following commands to create migrations and apply them:
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+# Then go to admin.py and register the models:
+from django.contrib import admin
+from .models import Topic, Message
+admin.site.register(Topic)
+admin.site.register(Message)
+
+
+
